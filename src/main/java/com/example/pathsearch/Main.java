60f6861265a02bd7233920a87c24040d10720810
Lin Chung Yi <eric@LindeMacBook-Pro.local>
2022-04-05 02:44:43 +0100

    Change starting window size

--- src/main/java/com/example/pathsearch/Main.java
+++ src/main/java/com/example/pathsearch/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Path Search");
         stage.setScene(scene);
         stage.show();
